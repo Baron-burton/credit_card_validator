c8c69f8fe0df0f9e5d878ffd79ba2eb0e69ff1d9
brandon <brandon.burton@thebeansgroup.com>
2019-06-21 17:13:50 +0100

    Validate Visa card numbers

--- app/actions/validate_credit_card_number.rb
+++ app/actions/validate_credit_card_number.rb
@@ -11,6 +11,8 @@ class ValidateCreditCardNumber
 
       if Mastercard.initial_digits.include?(initial_digits[0..1])
         Mastercard.card_length == card_number.length
+      elsif card_number.start_with?(Visa.initial_digits)
+        Visa.card_length.include?(card_number.length)
       else
         raise 'Not a valid card number'
       end
--- spec/actions/validate_credit_card_number_spec.rb
+++ spec/actions/validate_credit_card_number_spec.rb
@@ -20,5 +20,24 @@ RSpec.describe ValidateCreditCardNumber do
         end
       end
     end
+
+    context 'Visa' do
+      context 'valid credit card numnber' do
+        let(:card_number) { '4111111111111111' }
+
+        it 'validates the card number matches the Visa pattern' do
+          expect(described_class.run(card_number)).to be_truthy
+        end
+      end
+
+      context 'invalid credit card number' do
+        let(:card_number) { '1500000000000004' }
+
+        it 'returns an error' do
+          expect { described_class.run(card_number) }
+            .to raise_error('Not a valid card number')
+        end
+      end
+    end
   end
 end
