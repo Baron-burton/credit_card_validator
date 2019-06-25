93e5c8f1d1ddcbd693838d0d86f4d35629a93fc8
brandon <brandon.burton@thebeansgroup.com>
2019-06-25 20:40:33 +0100

    Move FindCardCompanyController out from Api namespace
    The original idea was to have this validator behave as an
    api returning a JSON response. This now behave like a normal
    controller that renders views

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do
-      resources :find_card_company, only: :index
-    end
-  end
+  resources :find_card_company, only: :index
 end
